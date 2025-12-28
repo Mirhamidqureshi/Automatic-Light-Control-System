4776caa2b23c4ed0ca4174ee97b9c9ae348650d4
Hamid Ali <168479004+Mirhamidqureshi@users.noreply.github.com>
2025-12-28 21:36:49 +0500

    Update automatic_light.ino

--- code/automatic_light.ino
+++ code/automatic_light.ino
@@ -1,1 +1,31 @@
+int pirPin = 2;
+int ldrPin = A0;
+int relayPin = 8;
+
+int ldrThreshold = 500;
+
+void setup() {
+  pinMode(pirPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int pirState = digitalRead(pirPin);
+  int ldrValue = analogRead(ldrPin);
+
+  Serial.print("PIR: ");
+  Serial.print(pirState);
+  Serial.print(" | LDR: ");
+  Serial.println(ldrValue);
+
+  if (pirState == HIGH && ldrValue < ldrThreshold) {
+    digitalWrite(relayPin, HIGH);
+  } else {
+    digitalWrite(relayPin, LOW);
+  }
+
+  delay(500);
+}
 
